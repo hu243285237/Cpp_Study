1ae85ca3f3c416dadce4f69c41efef7a5060b2b1
hu243285237 <243285237@qq.com>
2018-06-10 00:54:32 +0800

    toP51

---
+++ C++Book/P28.cpp
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////
+//	简单的C++输入输出例子
+//////////////////////////////////////////////////
+
+#pragma once
+
+#include<iostream>//输入输出流
+
+using namespace std;//std标准库\
+
+int _P28()
+{
+	int a = 1;
+	int b = 2;
+
+	int c = 0;
+
+	cout << a << b << c << endl;//输出 cout=console+out
+	cin >> c;//等待输入 cin=console+in
+
+	return 0;
+}
