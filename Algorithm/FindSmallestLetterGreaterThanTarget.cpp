d3897d20a3e4a5c5f8b38647fc89574f1a52d90a
24 <243285237@qq.com>
2018-11-11 22:31:59 +0800

    Create FindSmallestLetterGreaterThanTarget.cpp

---
+++ Algorithm/FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//  给定一个只包含小写字母的有序数组letters 和一个目标字母 target，
+//  寻找有序数组里面比目标字母大的最小字母。
+//  数组里字母的顺序是循环的。举个例子，如果目标字母target = 'z'
+//  并且有序数组为 letters = ['a', 'b']，则答案返回 'a'。
+///////////////////////////////////////////////////////////////////////////////////////////
+//  https://leetcode-cn.com/problems/find-smallest-letter-greater-than-target/description/
+///////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution 
+{
+    public:
+    
+    char nextGreatestLetter(vector<char>& letters, char target) 
+    {
+        char res = '~';
+        
+        for (char c : letters)
+        {
+            if (c > target && c - target < res - target)
+            {
+                res = c;
+            }
+        }
+        
+        return res == '~' ? letters[0] : res;
+    }
+};
