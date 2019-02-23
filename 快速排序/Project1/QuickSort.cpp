196475a8b7799dabf53f45026170256c026cdfff
hu243285237 <243285237@qq.com>
2019-02-23 10:18:20 +0800

    Finish QuickSort

--- 快速排序/Project1/QuickSort.cpp
+++ 快速排序/Project1/QuickSort.cpp
@@ -5,14 +5,15 @@
 //	是冒泡排序的改进版
 //
 //	最差时间复杂度为O(N平方)
-//	平均时间复杂度为O(NlogN)
+//	平均时间复杂度为O(Nlog2(N))
 //
 //	通过一轮的排序将序列分为独立的两部分，右边部分均比左边大
 //	重复上述步骤，对序列不断分割，以达到排序
 //	由于已经分开的两部分元素不需要再进行比较
 //	故减少了比较次数，降低了排序时间
 //	
-//	
+//	经过快速排序后两个相同值的相对位置可能会发生改变
+//	所以快速排序是一种不稳定排序
 //	
 ////////////////////////////////////////////////////////////
 
@@ -20,6 +21,9 @@
 #include<vector>
 
 using namespace std;
+
+//要先声明才能使用
+void quickSort(vector<int>& v, int left, int right);
 
 void main()
 {
@@ -31,13 +35,7 @@ void main()
 	//换两次行
 	cout << endl << endl;
 
-	/*for (int i = 0; i < v.size() - 1; i++)
-	{
-		for (int j = 0; j < v.size() - i - 1; j++)
-		{
-			if (v[j] > v[j + 1]) swap(v[j], v[j + 1]);
-		}
-	}*/
+	quickSort(v, 0, v.size() - 1);
 
 	//排序后
 	for (int i : v)	cout << i << ends;
@@ -46,9 +44,26 @@ void main()
 	getchar();
 }
 
-void swap(int& a, int& b)
+void quickSort(vector<int>& v, int originLeft, int originRight)
 {
-	int temp = a;
-	a = b;
-	b = temp;
+	int left = originLeft;
+	int right = originRight;
+
+	if (originLeft < originRight)
+	{
+		int base = v[left];
+
+		while (left != right)
+		{
+			while (right > left && v[right] >= base) right--;
+			v[left] = v[right];
+			while (right > left && v[left] <= base) left++;
+			v[right] = v[left];
+		}
+
+		v[left] = base;
+
+		quickSort(v, originLeft, left - 1);
+		quickSort(v, right + 1, originRight);
+	}
 }
